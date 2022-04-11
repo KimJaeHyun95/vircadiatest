1dd655006bf306907c617d1f5fc9ab52b0fc4f1b
재현 김 <kjh9527@esjee.co.kr>
2022-04-11 13:55:44 +0900

    edit

---
+++ serverScript.js
@@ -0,0 +1,15 @@
+console.log("Server Script Start!");
+
+Entities.editEntity("7d4a4b16-3cf3-4d9b-a7c9-356338b47ae2", {
+  Image: "https://raw.githubusercontent.com/KimJaeHyun95/vircadiatest/main/img/img01.jpeg"
+});
+
+Entities.editEntity("9e10720f-005d-41df-b695-b98ebc64d23b", {
+  img: "https://raw.githubusercontent.com/KimJaeHyun95/vircadiatest/main/img/img02.jpeg"
+});
+
+Entities.editEntity("eefc8e15-f962-47bb-ac58-d2330c9c5b0e", {
+  text: "true"
+});
+
+console.log("Server Script Finished!");
