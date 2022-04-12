a7d13d66c841eb049cb6c4a36640b1492ed2825b
재현 김 <kjh9527@esjee.co.kr>
2022-04-12 17:43:23 +0900

    edit

---
+++ serverScript2.js
@@ -0,0 +1,39 @@
+console.log("Server Script Start!");
+
+//모나리자
+Entities.editEntity("dd5d7267-57f6-437e-a8b5-1accf0d16aa4", {
+  "imageURL": "https://raw.githubusercontent.com/KimJaeHyun95/vircadiatest/main/img/img01.jpeg",
+  "script": "https://raw.githubusercontent.com/KimJaeHyun95/vircadiatest/main/clickToVisiable.js"
+});
+
+//모나리자 버튼
+Entities.editEntity("3d82196b-688d-4c9b-8a07-b0ea9244d33e", {
+  "script": "https://raw.githubusercontent.com/KimJaeHyun95/vircadiatest/main/clickToSound.js"
+});
+
+//천지창조
+Entities.editEntity("a52b7c88-ca65-4061-a0fd-38d8c8683eb9", {
+  "imageURL": "https://raw.githubusercontent.com/KimJaeHyun95/vircadiatest/main/img/img02.jpeg"
+});
+
+//천지창조 버튼
+Entities.editEntity("d8343313-71cf-4df9-8850-47231b40272e", {
+  "script": "https://raw.githubusercontent.com/KimJaeHyun95/vircadiatest/main/clickToChange.js"
+});
+
+//천지창조 웹
+Entities.editEntity("3a933a98-c575-4235-aba5-e4cc9f4c31a4", {
+  "sourceUrl": "https://www.youtube.com/watch?v=6EV-Ovs1zi4"
+});
+
+//진주귀걸이
+Entities.editEntity("86f8689d-4b50-4580-aa95-e3ae9aa1d6bf", {
+  "imageURL": "https://raw.githubusercontent.com/KimJaeHyun95/vircadiatest/main/img/img03.jpeg"
+});
+
+//배경음악
+Entities.editEntity("001a3c3c-f04c-405e-9190-587aa4416ff4", {
+  "script": "https://raw.githubusercontent.com/KimJaeHyun95/vircadiatest/main/explore.js"
+});
+
+console.log("Server Script Finished!");
